84aa701ad894e6022f504a0a071a2005c81f1254
Sushruth Shastry <shastry.sushruth@gmail.com>
2013-04-22 16:46:59 +0530

    Automatic change for the Ticker added

--- html/js/main.js
+++ html/js/main.js
@@ -1,14 +1,31 @@
 $(document).ready(function () {
 
 	$('#tickerUp').on('click', function () {
+		var margin = $('#tick').css('margin-top').replace('px', '');
+		var number = $('.tick').length;
+		number = (number - 1) * (-50) + 10;
+		if (margin > number) {
+			$('#tick').css({ marginTop: '-=50' });
+		} else {
+			$('#tick').css({ marginTop: 10 });
+		}
 
-		$('#tick').fadeOut(100);
 	});
+
+	setInterval(function () {
+		$('#tickerUp').trigger('click');
+	}, 1500);
 
 	$('#tickerDown').on('click', function () {
+		var margin = $('#tick').css('margin-top').replace('px', '');
+		var number = $('.tick').length;
+		number = (number - 1) * -50 + 10;
 
-		$('#tick').fadeIn(100);
+		if (margin < 10) {
+			$('#tick').css({ marginTop: '+=50' });
+		} else {
+			$('#tick').css({ marginTop: number });
+		}
+
 	});
-
-
 });
