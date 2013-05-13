87d2c6e240d04140de7e5268dadd29f58f4e51aa
Sushruth Shastry <shastry.sushruth@gmail.com>
2013-05-13 12:48:00 +0530

    Issues #2, #6 and #7 addressed.

--- html/js/main.js
+++ html/js/main.js
@@ -55,4 +55,34 @@ $(document).ready(function () {
 			$(this).val(local);
 		}
 	});
+	
+	// PLACEHOLDER FIX
+	// CREDITS : http://webdesignerwall.com/demo/html5-placeholder-text/
+	
+	if (!Modernizr.input.placeholder) {
+
+	    $('[placeholder]').focus(function () {
+	        var input = $(this);
+	        if (input.val() == input.attr('placeholder')) {
+	            input.val('');
+	            input.removeClass('placeholder');
+	        }
+	    }).blur(function () {
+	        var input = $(this);
+	        if (input.val() == '' || input.val() == input.attr('placeholder')) {
+	            input.addClass('placeholder');
+	            input.val(input.attr('placeholder'));
+	        }
+	    }).blur();
+	    $('[placeholder]').parents('form').submit(function () {
+	        $(this).find('[placeholder]').each(function () {
+	            var input = $(this);
+	            if (input.val() == input.attr('placeholder')) {
+	                input.val('');
+	            }
+	        })
+	    });
+
+	}
+	
 });
